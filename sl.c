2afa4c228dc16855c1f8b651bcaf81e14e7c6a53
Taichi Sugiyama <taichi_sugiyama@dwango.co.jp>
2015-11-02 23:15:11 +0900

    えるたそ〜

--- sl.c
+++ sl.c
@@ -6,6 +6,9 @@
  *	Last Modified: 2013/ 5/ 5
  *========================================
  */
+/* sl version 5.1e : mayakatasonize                                          */
+/*                   restore -l -c options.                                  */
+/*                                             by Taichi Sugiyama 2014/ 6/10 */
 /* sl version 5.0e : erutasonize                                             */
 /*                   delete -l -c options.                                   */
 /*                                             by Taichi Sugiyama 2014/ 3/26 */
@@ -42,6 +45,8 @@
 
 int ACCIDENT  = 0;
 int FLY       = 0;
+int MAYAKA    = 0;
+int LOLI    = 0;
 
 int my_mvaddstr(int y, int x, char *str)
 {
@@ -54,14 +59,16 @@ int my_mvaddstr(int y, int x, char *str)
 
 void option(char *str)
 {
-    extern int ACCIDENT, FLY;
+    extern int ACCIDENT, FLY, LOLI, MAYAKA;
 
     while (*str != '\0') {
-	switch (*str++) {
-	    case 'a': ACCIDENT = 1; break;
-	    case 'F': FLY      = 1; break;
-	    default:                break;
-	}
+		switch (*str++) {
+		    case 'a': ACCIDENT = 1; break;
+		    case 'F': FLY      = 1; break;
+		    case 'l': LOLI   = 1; break;
+		    case 'c': MAYAKA   = 1; break;
+		    default:                break;
+		}
     }
 }
 
@@ -83,10 +90,18 @@ int main(int argc, char *argv[])
     scrollok(stdscr, FALSE);
 
     for (x = COLS - 1; ; --x) {
-	if (add_erutaso(x) == ERR) break;
-    getch();
-	refresh();
-	usleep(40000);
+        if (LOLI == 1) {
+            if (add_lolitaso(x) == ERR) break;
+        }
+        else if (MAYAKA == 1) {
+            if (add_mayakataso(x) == ERR) break;
+        }
+        else {
+            if (add_erutaso(x) == ERR) break;
+        }
+        getch();
+        refresh();
+        usleep(40000);
     }
     mvcur(0, COLS - 1, LINES - 1, 0);
     endwin();
@@ -115,17 +130,86 @@ int add_erutaso(int x)
 	my_mvaddstr(y + i, x, erutaso[(ERUTASOLENGTH + x) / 3 % ERUTASOPATTERNS][i]);
     }
     if (ACCIDENT == 1) {
-	add_kininarimasu(y , x + 16);
+		add_kininarimasu(y , x + 16);
     }
     add_smoke(y - 1, x + ERUTASOFUNNEL);
     return OK;
 }
 
 
+int add_mayakataso(int x)
+{
+    static char *mayakataso[MAYAKATASOPATTERNS][MAYAKATASOHIGHT + 1]
+	= {{MAYAKATASO11, MAYAKATASO12, MAYAKATASO13, MAYAKATASO14, MAYAKATASO15, MAYAKATASO16, MAYAKATASO17, MAYAKATASO18, MAYAKATASO19, MAYAKATASO1DEL},
+	   {MAYAKATASO21, MAYAKATASO22, MAYAKATASO23, MAYAKATASO24, MAYAKATASO25, MAYAKATASO26, MAYAKATASO27, MAYAKATASO28, MAYAKATASO29, MAYAKATASO2DEL},
+	   {MAYAKATASO31, MAYAKATASO32, MAYAKATASO33, MAYAKATASO34, MAYAKATASO35, MAYAKATASO36, MAYAKATASO37, MAYAKATASO38, MAYAKATASO39, MAYAKATASO3DEL},
+	   {MAYAKATASO41, MAYAKATASO42, MAYAKATASO43, MAYAKATASO44, MAYAKATASO45, MAYAKATASO46, MAYAKATASO47, MAYAKATASO48, MAYAKATASO49, MAYAKATASO4DEL},
+	   {MAYAKATASO31, MAYAKATASO32, MAYAKATASO33, MAYAKATASO34, MAYAKATASO35, MAYAKATASO36, MAYAKATASO37, MAYAKATASO38, MAYAKATASO39, MAYAKATASO3DEL},
+	   {MAYAKATASO21, MAYAKATASO22, MAYAKATASO23, MAYAKATASO24, MAYAKATASO25, MAYAKATASO26, MAYAKATASO27, MAYAKATASO28, MAYAKATASO29, MAYAKATASO2DEL}};
+
+    int i, y;
+
+    if (x < - MAYAKATASOLENGTH)  return ERR;
+    y = LINES / 2 - 3;
+
+    if (FLY == 1) {
+	y = (x / 6) + LINES - (COLS / 6) - MAYAKATASOHIGHT;
+    }
+    for (i = 0; i <= MAYAKATASOHIGHT; ++i) {
+	my_mvaddstr(y + i, x, mayakataso[(MAYAKATASOLENGTH + x) / 3 % MAYAKATASOPATTERNS][i]);
+    }
+    if (ACCIDENT == 1) {
+		add_fukuchan(y , x + 16);
+    }
+    add_smoke(y - 1, x + MAYAKATASOFUNNEL);
+    return OK;
+}
+
+
+int add_lolitaso(int x)
+{
+    static char *LOLITASO[LOLITASOPATTERNS][LOLITASOHIGHT + 1]
+	= {{LOLITASO11, LOLITASO12, LOLITASO13, LOLITASO14, LOLITASO15, LOLITASO16, LOLITASO17, LOLITASO1DEL},
+	   {LOLITASO21, LOLITASO22, LOLITASO23, LOLITASO24, LOLITASO25, LOLITASO26, LOLITASO27, LOLITASO2DEL},
+	   {LOLITASO31, LOLITASO32, LOLITASO33, LOLITASO34, LOLITASO35, LOLITASO36, LOLITASO37, LOLITASO3DEL},
+	   {LOLITASO41, LOLITASO42, LOLITASO43, LOLITASO44, LOLITASO45, LOLITASO46, LOLITASO47, LOLITASO4DEL},
+	   {LOLITASO31, LOLITASO32, LOLITASO33, LOLITASO34, LOLITASO35, LOLITASO36, LOLITASO37, LOLITASO3DEL},
+	   {LOLITASO21, LOLITASO22, LOLITASO23, LOLITASO24, LOLITASO25, LOLITASO26, LOLITASO27, LOLITASO2DEL}};
+
+    int i, y;
+
+    if (x < - LOLITASOLENGTH)  return ERR;
+    y = LINES / 2 - 3;
+
+    if (FLY == 1) {
+	y = (x / 6) + LINES - (COLS / 6) - LOLITASOHIGHT;
+    }
+    for (i = 0; i <= LOLITASOHIGHT; ++i) {
+	my_mvaddstr(y + i, x, LOLITASO[(LOLITASOLENGTH + x) / 3 % LOLITASOPATTERNS][i]);
+    }
+    if (ACCIDENT == 1) {
+		add_lolinarimasu(y , x + 13);
+    }
+    add_smoke(y - 1, x + LOLITASOFUNNEL);
+    return OK;
+}
+
 int add_kininarimasu(int y, int x)
 {
 	static char *man[2] = {"", "< KININARIMASU! "};
 	my_mvaddstr(y , x, man[(ERUTASOLENGTH + x) / 12 % 2]);
+}
+
+int add_fukuchan(int y, int x)
+{
+	static char *man[2] = {"", "< FUKUCHAN! "};
+	my_mvaddstr(y , x, man[(MAYAKATASOLENGTH + x) / 12 % 2]);
+}
+
+int add_lolinarimasu(int y, int x)
+{
+	static char *man[2] = {"", "< kininarimasu! "};
+	my_mvaddstr(y , x, man[(LOLITASOLENGTH + x) / 12 % 2]);
 }
 
 
--- sl.h
+++ sl.h
@@ -49,7 +49,7 @@
 
 #define ERUTASO41  "      _______                       "
 #define ERUTASO42  "    /:::::::::\\   __                "
-#define ERUTASO43  "  \\/::::::::::::\\/ _)               "
+#define ERUTASO43  "  |/::::::::::::\\/ _)               "
 #define ERUTASO44  "  |::_A__A_:::::| _)                "
 #define ERUTASO45  "  |:| O   O |:::|_)                 "
 #define ERUTASO46  "  l:( \" _ \" |:|:|)                  "
@@ -57,4 +57,95 @@
 #define ERUTASO48  "    l:/\\     l:/ )                  "
 #define ERUTASO49  "        `~U^^^U~/                   "
 #define ERUTASO4DEL "                                    "
- 
+
+
+#define MAYAKATASOHIGHT     9
+#define MAYAKATASOFUNNEL    4
+#define MAYAKATASOLENGTH   36
+#define MAYAKATASOPATTERNS  6
+
+#define MAYAKATASO11  "      ______                        "
+#define MAYAKATASO12  " _  /::::::::\\  ______              "
+#define MAYAKATASO13  "( \\/:/\\/\\/\\:::\\/     _)             "
+#define MAYAKATASO14  " (/:/ ^   ^\\:::\\   _)               "
+#define MAYAKATASO15  " {::| O   O |:|:}_)                 "
+#define MAYAKATASO16  " V\\:( \" _ \" |:|:)                   "
+#define MAYAKATASO17  "   V C ---C  V\\/                    "
+#define MAYAKATASO18  "      \\       )                     "
+#define MAYAKATASO19  "       `~U^^U~/                     "
+#define MAYAKATASO1DEL "                                    "
+
+#define MAYAKATASO21  "      _______                       "
+#define MAYAKATASO22  "    /::::::::\\  ____                "
+#define MAYAKATASO23  " (\\/:/\\/\\/\\:::\\/   _)               "
+#define MAYAKATASO24  "  /:/ ^   ^\\:::\\ _)                 "
+#define MAYAKATASO25  " {::| O   O |:|:})                  "
+#define MAYAKATASO26  " V\\:( \" _ \" |:|:)                   "
+#define MAYAKATASO27  "   V C ---C  V\\/                    "
+#define MAYAKATASO28  "      \\       )                     "
+#define MAYAKATASO29  "       `~U^^U~/                     "
+#define MAYAKATASO2DEL "                                    "
+
+#define MAYAKATASO31  "      _______                       "
+#define MAYAKATASO32  "    /::::::::\\  __                  "
+#define MAYAKATASO33  "  \\/:/\\/\\/\\:::\\/ _)                 "
+#define MAYAKATASO34  "  /:/ ^   ^\\:::\\_)                  "
+#define MAYAKATASO35  " {::| O   O |:|:}                   "
+#define MAYAKATASO36  " V\\:( \" _ \" |:|:)                   "
+#define MAYAKATASO37  "   V C ---C  V\\/                    "
+#define MAYAKATASO38  "      \\       )                     "
+#define MAYAKATASO39  "       `~U^^U~/                     "
+#define MAYAKATASO3DEL "                                    "
+
+#define MAYAKATASO41  "      _______                       "
+#define MAYAKATASO42  "    /::::::::\\  _                   "
+#define MAYAKATASO43  "  |/:/\\/\\/\\:::\\/_)                  "
+#define MAYAKATASO44  "  /:/ ^   ^\\:::\\)                   "
+#define MAYAKATASO45  " {::| O   O |:|:}                   "
+#define MAYAKATASO46  " V\\:( \" _ \" |:|:)                   "
+#define MAYAKATASO47  "   V C ---C  V\\/                    "
+#define MAYAKATASO48  "      \\       )                     "
+#define MAYAKATASO49  "       `~U^^U~/                     "
+#define MAYAKATASO4DEL "                                    "
+
+
+#define LOLITASOHIGHT    	 7
+#define LOLITASOFUNNEL  	 4
+#define LOLITASOLENGTH      36
+#define LOLITASOPATTERNS	 6
+
+#define LOLITASO11  "    ______                           "
+#define LOLITASO12  "   /::::::\\ ____                     "
+#define LOLITASO13  " (|:A::A:::|   _)                    "
+#define LOLITASO14  "  ||o  o |:|  _)                     "
+#define LOLITASO15  "  l(\"__\" |:|_)                       "
+#define LOLITASO16  "   l:c--cl:|                         "
+#define LOLITASO17  "     lu~u~/                          "
+#define LOLITASO1DEL "                                     "
+
+#define LOLITASO21  "    ______                           "
+#define LOLITASO22  "   /::::::\\ ___                      "
+#define LOLITASO23  " (|:A::A:::|  _)                     "
+#define LOLITASO24  "  ||o  o |:| _)                      "
+#define LOLITASO25  "  l(\"__\" |:|_)                       "
+#define LOLITASO26  "   l:c--cl:|                         "
+#define LOLITASO27  "     lu~u~/                          "
+#define LOLITASO2DEL "                                     "
+
+#define LOLITASO31  "    ______                           "
+#define LOLITASO32  "   /::::::\\ __                       "
+#define LOLITASO33  "  |:A::A:::| _)                      "
+#define LOLITASO34  "  ||o  o |:|_)                       "
+#define LOLITASO35  "  l(\"__\" |:|)                        "
+#define LOLITASO36  "   l:c--cl:|                         "
+#define LOLITASO37  "     lu~u~/                          "
+#define LOLITASO3DEL "                                     "
+
+#define LOLITASO41  "    ______                           "
+#define LOLITASO42  "   /::::::\\ _                        "
+#define LOLITASO43  "  |:A::A:::|_)                       "
+#define LOLITASO44  "  ||o  o |:|)                        "
+#define LOLITASO45  "  l(\"__\" |:|                         "
+#define LOLITASO46  "   l:c--cl:|                         "
+#define LOLITASO47  "     lu~u~/                          "
+#define LOLITASO4DEL "                                     "
